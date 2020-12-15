e9b9d9ac42f59b391884949f3fc7eb8a81abb277
Rui Gonçalves <rgoncalves0320@gmail.com>
2020-12-15 14:23:17 +0000

    docker

--- build/scripts/build.sh
+++ build/scripts/build.sh
@@ -12,8 +12,8 @@ then
 fi
 
 # Build
-sudo docker build -o ./build/client/web -f client - < ./build/scripts/Dockerfile.client
-sudo docker build -o ./build/server/server - < ./build/scripts/Dockerfile.server
+sudo docker build -o ./build/client/web -f ./build/scripts/Dockerfile.client ./client
+sudo docker build -o ./build/server/server -f ./build/scripts/Dockerfile.server ./server
 
 # Zip
 cp LICENSE build/
